1a724f6dea600147049e3e61709ce507cb9aa9be
Peter Hanecak <peter.hanecak@maptiler.com>
2024-11-14 15:17:53 +0100

    @ParameterizedTest+@CsvSource used to simplify the tests

--- src/test/java/fr/free/nrw/jakaroma/JakaromaTest.java
+++ src/test/java/fr/free/nrw/jakaroma/JakaromaTest.java
@@ -3,58 +3,68 @@ package fr.free.nrw.jakaroma;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class JakaromaTest {
 
     private final Jakaroma instance = new Jakaroma();
 
-    @Test
-    public void translate() {
-        String[] testInput = {"もらった", "ホッ", "すごっ", "ピッザ"};
-
-        assertEquals("Moratta", instance.convert(testInput[0], false, true));
-        assertEquals("Ho!", instance.convert(testInput[1], false, true));
-        assertEquals("Sug!", instance.convert(testInput[2], false, true));
-        assertEquals("Pizza", instance.convert(testInput[3], false, true));
+    @ParameterizedTest
+    @CsvSource({
+        "もらった, Moratta",
+        "ホッ, Ho!",
+        "すごっ, Sug!",
+        "ピッザ, Pizza"
+    })
+    public void translate(String in, String out) {
+        assertEquals(out, instance.convert(in, false, true));
     }
 
-    @Test
-    public void noTranslateForLatin() {
-        String[] testInput = {"cat", "dog", "Hello!"};
-
-        assertEquals("Cat", instance.convert(testInput[0], false, true));
-        assertEquals("Dog", instance.convert(testInput[1], false, true));
-        assertEquals("Hello!", instance.convert(testInput[2], false, true));
+    @ParameterizedTest
+    @CsvSource({
+        "cat, Cat",
+        "dog, Dog",
+        "Hello!, Hello!"
+    })
+    public void noTranslateForLatin(String in, String out) {
+        assertEquals(out, instance.convert(in, false, true));
     }
 
-    @Test
-    public void yetAnotherTranslate() {
-        String[] testInput = {"小学校", "昨夜", "証書", "正直", "三日間", "順調", "任意", "爪", "みっかかん", "さっぱり",
-        "ばっちり", "じゅんちょうに", "ジャガイモ", "ニンジン", "ワーフル", "ウェファ", "フィリピン", "プライバシー", "バッチリ", "ジュンチャン",
-                "祐介さんのモットーは「ｙｅｓ，　ｗｅ　ｃａｎ」と「ウィンブレドンや全日本大会で男女両方ともが勝つ」"};
+    @ParameterizedTest
+    @CsvSource({
+        "小学校, Shougakkou",
+        "昨夜, Sakuya",
+        "証書, Shousho",
+        "正直, Shoujiki",
+        "三日間, 三Nichikan",
+        "順調, Junchou",
+        "任意, Nini",
+        "爪, Tsume",
+        "みっかかん, Mikkakan",
+        "さっぱり, Sappari",
+        "ばっちり, BacchiRi",
+        "じゅんちょうに, JunChouNi",
+        "ジャガイモ, Jagaimo",
+        "ニンジン, Ninjin",
+        "ワーフル, Wa-furu",
+        "ウェファ, Uェfuァ",
+        "フィリピン, Firipin",
+        "プライバシー, Puraibashi-",
+        "バッチリ, Bacchiri",
+        "ジュンチャン, JunChan"
+    })
+    public void yetAnotherTranslate(String in, String out) {
+        assertEquals(out, instance.convert(in, false, true));
+    }
 
-        assertEquals("Shougakkou", instance.convert(testInput[0], false, true));
-        assertEquals("Sakuya", instance.convert(testInput[1], false, true));
-        assertEquals("Shousho", instance.convert(testInput[2], false, true));
-        assertEquals("Shoujiki", instance.convert(testInput[3], false, true));
-        assertEquals("三Nichikan", instance.convert(testInput[4], false, true));
-        assertEquals("Junchou", instance.convert(testInput[5], false, true));
-        assertEquals("Nini", instance.convert(testInput[6], false, true));
-        assertEquals("Tsume", instance.convert(testInput[7], false, true));
-        assertEquals("Mikkakan", instance.convert(testInput[8], false, true));
-        assertEquals("Sappari", instance.convert(testInput[9], false, true));
-        assertEquals("BacchiRi", instance.convert(testInput[10], false, true));
-        assertEquals("JunChouNi", instance.convert(testInput[11], false, true));
-        assertEquals("Jagaimo", instance.convert(testInput[12], false, true));
-        assertEquals("Ninjin", instance.convert(testInput[13], false, true));
-        assertEquals("Wa-furu", instance.convert(testInput[14], false, true));
-        assertEquals("Uェfuァ", instance.convert(testInput[15], false, true));
-        assertEquals("Firipin", instance.convert(testInput[16], false, true));
-        assertEquals("Puraibashi-", instance.convert(testInput[17], false, true));
-        assertEquals("Bacchiri", instance.convert(testInput[18], false, true));
-        assertEquals("JunChan", instance.convert(testInput[19], false, true));
-        assertEquals("Yuusuke San No Motto- Ha \"Yes , We  Can \"To \"Uィnburedon Ya Zennihon Taikai De Danjo Ryouhou Tomo Ga Katsu \"", instance.convert(testInput[20], true, true));
-        assertEquals(".;\"\"", instance.convert("。；「」", true, true));
+    @ParameterizedTest
+    @CsvSource({
+        "祐介さんのモットーは「ｙｅｓ，　ｗｅ　ｃａｎ」と「ウィンブレドンや全日本大会で男女両方ともが勝つ」, 'Yuusuke San No Motto- Ha \"Yes , We  Can \"To \"Uィnburedon Ya Zennihon Taikai De Danjo Ryouhou Tomo Ga Katsu \"'",
+        "。；「」, .;\"\""
+    })
+    public void andYetAnotherTranslate(String in, String out) {
+        assertEquals(out, instance.convert(in, true, true));
     }
 
     @Test
--- src/test/java/fr/free/nrw/jakaroma/KanaToRomajiTest.java
+++ src/test/java/fr/free/nrw/jakaroma/KanaToRomajiTest.java
@@ -2,38 +2,29 @@ package fr.free.nrw.jakaroma;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class KanaToRomajiTest {
 
     private final KanaToRomaji k2r = new KanaToRomaji();
 
-    @Test
-    public void translate() {
-        String[] strs = {"ピュートフクジャガー",
-                "マージャン",
-                "タンヤオトイトイドラドラ",
-                "キップ",
-                "プリキュア",
-                "シャーペン",
-                "カプッ",
-                "@マーク",
-                "ティーカップ",
-                "ビルディング",
-                "ロッポンギヒルズ",
-                "トッツィ"};
-
-        assertEquals("pyu-tofukujaga-", k2r.convert(strs[0]));
-        assertEquals("ma-jan", k2r.convert(strs[1]));
-        assertEquals("tanyaotoitoidoradora", k2r.convert(strs[2]));
-        assertEquals("kippu", k2r.convert(strs[3]));
-        assertEquals("purikyua", k2r.convert(strs[4]));
-        assertEquals("sha-pen", k2r.convert(strs[5]));
-        assertEquals("kapuッ", k2r.convert(strs[6]));
-        assertEquals("@ma-ku", k2r.convert(strs[7]));
-        assertEquals("ti-kappu", k2r.convert(strs[8]));
-        assertEquals("birudingu", k2r.convert(strs[9]));
-        assertEquals("roppongihiruzu", k2r.convert(strs[10]));
-        assertEquals("tottsi", k2r.convert(strs[11]));
+    @ParameterizedTest
+    @CsvSource({
+        "ピュートフクジャガー, pyu-tofukujaga-",
+        "マージャン, ma-jan",
+        "タンヤオトイトイドラドラ, tanyaotoitoidoradora",
+        "キップ, kippu",
+        "プリキュア, purikyua",
+        "シャーペン, sha-pen",
+        "カプッ, kapuッ",
+        "@マーク, @ma-ku",
+        "ティーカップ, ti-kappu",
+        "ビルディング, birudingu",
+        "ロッポンギヒルズ, roppongihiruzu",
+        "トッツィ, tottsi"
+    })
+    public void translate(String in, String out) {
+        assertEquals(out, k2r.convert(in));
     }
 }
