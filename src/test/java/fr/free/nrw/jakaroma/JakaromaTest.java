1bf0ef3220058dd92453aede8607e1d90ab7b974
Peter Hanecak <peter.hanecak@maptiler.com>
2024-11-14 15:14:29 +0100

    JUnit updated from 4.13.1 to 5.11.3

--- src/test/java/fr/free/nrw/jakaroma/JakaromaTest.java
+++ src/test/java/fr/free/nrw/jakaroma/JakaromaTest.java
@@ -1,7 +1,8 @@
 package fr.free.nrw.jakaroma;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class JakaromaTest {
 
@@ -11,19 +12,19 @@ public class JakaromaTest {
     public void translate() {
         String[] testInput = {"もらった", "ホッ", "すごっ", "ピッザ"};
 
-        Assert.assertEquals("Moratta", instance.convert(testInput[0], false, true));
-        Assert.assertEquals("Ho!", instance.convert(testInput[1], false, true));
-        Assert.assertEquals("Sug!", instance.convert(testInput[2], false, true));
-        Assert.assertEquals("Pizza", instance.convert(testInput[3], false, true));
+        assertEquals("Moratta", instance.convert(testInput[0], false, true));
+        assertEquals("Ho!", instance.convert(testInput[1], false, true));
+        assertEquals("Sug!", instance.convert(testInput[2], false, true));
+        assertEquals("Pizza", instance.convert(testInput[3], false, true));
     }
 
     @Test
     public void noTranslateForLatin() {
         String[] testInput = {"cat", "dog", "Hello!"};
 
-        Assert.assertEquals("Cat", instance.convert(testInput[0], false, true));
-        Assert.assertEquals("Dog", instance.convert(testInput[1], false, true));
-        Assert.assertEquals("Hello!", instance.convert(testInput[2], false, true));
+        assertEquals("Cat", instance.convert(testInput[0], false, true));
+        assertEquals("Dog", instance.convert(testInput[1], false, true));
+        assertEquals("Hello!", instance.convert(testInput[2], false, true));
     }
 
     @Test
@@ -32,40 +33,38 @@ public class JakaromaTest {
         "ばっちり", "じゅんちょうに", "ジャガイモ", "ニンジン", "ワーフル", "ウェファ", "フィリピン", "プライバシー", "バッチリ", "ジュンチャン",
                 "祐介さんのモットーは「ｙｅｓ，　ｗｅ　ｃａｎ」と「ウィンブレドンや全日本大会で男女両方ともが勝つ」"};
 
-        Assert.assertEquals("Shougakkou", instance.convert(testInput[0], false, true));
-        Assert.assertEquals("Sakuya", instance.convert(testInput[1], false, true));
-        Assert.assertEquals("Shousho", instance.convert(testInput[2], false, true));
-        Assert.assertEquals("Shoujiki", instance.convert(testInput[3], false, true));
-        Assert.assertEquals("三Nichikan", instance.convert(testInput[4], false, true));
-        Assert.assertEquals("Junchou", instance.convert(testInput[5], false, true));
-        Assert.assertEquals("Nini", instance.convert(testInput[6], false, true));
-        Assert.assertEquals("Tsume", instance.convert(testInput[7], false, true));
-        Assert.assertEquals("Mikkakan", instance.convert(testInput[8], false, true));
-        Assert.assertEquals("Sappari", instance.convert(testInput[9], false, true));
-        Assert.assertEquals("BacchiRi", instance.convert(testInput[10], false, true));
-        Assert.assertEquals("JunChouNi", instance.convert(testInput[11], false, true));
-        Assert.assertEquals("Jagaimo", instance.convert(testInput[12], false, true));
-        Assert.assertEquals("Ninjin", instance.convert(testInput[13], false, true));
-        Assert.assertEquals("Wa-furu", instance.convert(testInput[14], false, true));
-        Assert.assertEquals("Uェfuァ", instance.convert(testInput[15], false, true));
-        Assert.assertEquals("Firipin", instance.convert(testInput[16], false, true));
-        Assert.assertEquals("Puraibashi-", instance.convert(testInput[17], false, true));
-        Assert.assertEquals("Bacchiri", instance.convert(testInput[18], false, true));
-        Assert.assertEquals("JunChan", instance.convert(testInput[19], false, true));
-        Assert.assertEquals("Yuusuke San No Motto- Ha \"Yes , We  Can \"To \"Uィnburedon Ya Zennihon Taikai De Danjo Ryouhou Tomo Ga Katsu \"", instance.convert(testInput[20], true, true));
-        Assert.assertEquals(".;\"\"", instance.convert("。；「」", true, true));
+        assertEquals("Shougakkou", instance.convert(testInput[0], false, true));
+        assertEquals("Sakuya", instance.convert(testInput[1], false, true));
+        assertEquals("Shousho", instance.convert(testInput[2], false, true));
+        assertEquals("Shoujiki", instance.convert(testInput[3], false, true));
+        assertEquals("三Nichikan", instance.convert(testInput[4], false, true));
+        assertEquals("Junchou", instance.convert(testInput[5], false, true));
+        assertEquals("Nini", instance.convert(testInput[6], false, true));
+        assertEquals("Tsume", instance.convert(testInput[7], false, true));
+        assertEquals("Mikkakan", instance.convert(testInput[8], false, true));
+        assertEquals("Sappari", instance.convert(testInput[9], false, true));
+        assertEquals("BacchiRi", instance.convert(testInput[10], false, true));
+        assertEquals("JunChouNi", instance.convert(testInput[11], false, true));
+        assertEquals("Jagaimo", instance.convert(testInput[12], false, true));
+        assertEquals("Ninjin", instance.convert(testInput[13], false, true));
+        assertEquals("Wa-furu", instance.convert(testInput[14], false, true));
+        assertEquals("Uェfuァ", instance.convert(testInput[15], false, true));
+        assertEquals("Firipin", instance.convert(testInput[16], false, true));
+        assertEquals("Puraibashi-", instance.convert(testInput[17], false, true));
+        assertEquals("Bacchiri", instance.convert(testInput[18], false, true));
+        assertEquals("JunChan", instance.convert(testInput[19], false, true));
+        assertEquals("Yuusuke San No Motto- Ha \"Yes , We  Can \"To \"Uィnburedon Ya Zennihon Taikai De Danjo Ryouhou Tomo Ga Katsu \"", instance.convert(testInput[20], true, true));
+        assertEquals(".;\"\"", instance.convert("。；「」", true, true));
     }
 
     @Test
     public void translateFullWidthToHalfWidth() {
-        Assert.assertEquals("wifi", instance.convert("ｗｉｆｉ", false, false));
+        assertEquals("wifi", instance.convert("ｗｉｆｉ", false, false));
     }
 
     @Test
     public void translateOsmWay971134980() {
         // see https://www.openstreetmap.org/way/971134980
-        String testInput = "キッッズスクール加古川つばめ保育園";
-
-        Assert.assertEquals("Kizzusuku-ruKakogawaTsubaMeHoikuen", instance.convert(testInput, false, true));
+        assertEquals("Kizzusuku-ruKakogawaTsubaMeHoikuen", instance.convert("キッッズスクール加古川つばめ保育園", false, true));
     }
 }
--- src/test/java/fr/free/nrw/jakaroma/KanaToRomajiTest.java
+++ src/test/java/fr/free/nrw/jakaroma/KanaToRomajiTest.java
@@ -1,7 +1,8 @@
 package fr.free.nrw.jakaroma;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class KanaToRomajiTest {
 
@@ -22,17 +23,17 @@ public class KanaToRomajiTest {
                 "ロッポンギヒルズ",
                 "トッツィ"};
 
-        Assert.assertEquals("pyu-tofukujaga-", k2r.convert(strs[0]));
-        Assert.assertEquals("ma-jan", k2r.convert(strs[1]));
-        Assert.assertEquals("tanyaotoitoidoradora", k2r.convert(strs[2]));
-        Assert.assertEquals("kippu", k2r.convert(strs[3]));
-        Assert.assertEquals("purikyua", k2r.convert(strs[4]));
-        Assert.assertEquals("sha-pen", k2r.convert(strs[5]));
-        Assert.assertEquals("kapuッ", k2r.convert(strs[6]));
-        Assert.assertEquals("@ma-ku", k2r.convert(strs[7]));
-        Assert.assertEquals("ti-kappu", k2r.convert(strs[8]));
-        Assert.assertEquals("birudingu", k2r.convert(strs[9]));
-        Assert.assertEquals("roppongihiruzu", k2r.convert(strs[10]));
-        Assert.assertEquals("tottsi", k2r.convert(strs[11]));
+        assertEquals("pyu-tofukujaga-", k2r.convert(strs[0]));
+        assertEquals("ma-jan", k2r.convert(strs[1]));
+        assertEquals("tanyaotoitoidoradora", k2r.convert(strs[2]));
+        assertEquals("kippu", k2r.convert(strs[3]));
+        assertEquals("purikyua", k2r.convert(strs[4]));
+        assertEquals("sha-pen", k2r.convert(strs[5]));
+        assertEquals("kapuッ", k2r.convert(strs[6]));
+        assertEquals("@ma-ku", k2r.convert(strs[7]));
+        assertEquals("ti-kappu", k2r.convert(strs[8]));
+        assertEquals("birudingu", k2r.convert(strs[9]));
+        assertEquals("roppongihiruzu", k2r.convert(strs[10]));
+        assertEquals("tottsi", k2r.convert(strs[11]));
     }
 }
